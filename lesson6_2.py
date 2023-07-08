5aba994f27214f63f3117ae6d88b09b33e8f4306
YuChuanLiao <133958859+YuChuanLiao@users.noreply.github.com>
2023-07-08 07:39:47 +0000

    modify

--- lesson6_2.py
+++ lesson6_2.py
@@ -3,10 +3,10 @@
 import random
 
 min = 1
-max = 10
+max = 100
 count = 0
 target = random.randint(min, max)
-print("==========拆數字遊戲==========")
+print("==========拆數字遊戲==========\n\n")
 
 while True:
     keyin = int(input(f"拆數字範圍{min}~{max}："))
@@ -15,7 +15,11 @@ while True:
         print(f"答對了!!答案是：{target}")
         print(f"您總共猜了{count}次")
         break
-    else:
-        print("猜錯囉，再來一次")
-        print(f"您已經猜了{count}次")
+    elif keyin > target:
+        print(f"數字比{keyin}小")
+        max = keyin - 1
+    elif keyin < target:
+        print(f"數字比{keyin}大")
+        min = keyin + 1
+    print(f"您已經猜了{count}次")
 print("遊戲結束!!!")
