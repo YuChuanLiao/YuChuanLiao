0839684566432b5aa50434c9350088a55e47930f
YuChuanLiao <133958859+YuChuanLiao@users.noreply.github.com>
2023-08-09 16:14:01 +0000

    modify

--- lesson13_3.py
+++ lesson13_3.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import streamlit as st
+
 t = np.arange(0.,1.0,0.05)
-print(t)
+st.write(t)
 y1 = np.sin(2 * np.pi * t)
 y2 = np.cos(2 * np.pi * t)
 print(y1)
@@ -10,4 +12,6 @@ figure1 = plt.figure(figsize=(8,4))
 axes1 = figure1.add_subplot()
 axes1.plot(y1)
 axes1.plot(y2)
-plt.show()
+
+
+st.write(figure1)
